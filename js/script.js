74a70c8294afebe7ca823ad31d90ae8ddea9e0fc
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2023-01-11 14:46:49 -0500

    started flowchart

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023 Claire Bedrossian All rights reserved
 //
 // Created by: Claire Bedrossian
-// Created on: Jan 2022
+// Created on: Jan 2023
 // This file contains the JS functions for index.html
 
 "use strict"
